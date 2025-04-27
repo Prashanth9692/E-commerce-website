990d6c32114512feb68f97681fb0c66cc0e1c1f7
Prashanth Patil <Prashantakumar.patil@gmail.com>
2025-04-27 18:34:45 +0530

    First commit for my E-commerce website

---
+++ Shopping/script.js
@@ -0,0 +1,7 @@
+// Example JS for any interactivity
+document.querySelectorAll('.product-card button').forEach(button => {
+    button.addEventListener('click', () => {
+      alert("Item added to cart!");
+    });
+  });
+  
